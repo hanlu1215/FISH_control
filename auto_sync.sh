a3e4c64d85906d50cc3609c2e7d4bfc3e283e7ef
韩路路 <hanlu0306@hotmail.com>
2025-03-29 21:41:11 +0800

    在自动同步脚本中添加退出提示，等待用户按键后退出

--- auto_sync.sh
+++ auto_sync.sh
@@ -5,3 +5,4 @@ git reset --hard HEAD  # ‌:ml-citation{ref="2,4" data="citationList"}
 git clean -df          # ‌:ml-citation{ref="3,4" data="citationList"}
 # 拉取远程最新代码并自动合并
 git pull               # ‌:ml-citation{ref="2,3" data="citationList"}
+read -n 1 -s -r -p "Press any key to exit..."
